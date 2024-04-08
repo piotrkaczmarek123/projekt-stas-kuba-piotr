9854dc296eb4efa90af7574ad0d94ae5296a9e12
unknown <user@V15O.edu2.zsl.poznan.pl>
2024-04-08 15:40:04 +0200

    created login-user view

--- proj2_login/backend/notes/urls.py
+++ proj2_login/backend/notes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import NoteView, UserView, CreateUserView, CreateNoteView
+from .views import NoteView, UserView, CreateUserView, CreateNoteView, LoginUserView
 
 urlpatterns = [
     path("notes", NoteView.as_view()),
     path("users", UserView.as_view()),
     path("create-note", CreateNoteView.as_view()),
     path("create-user", CreateUserView.as_view()),
+    path("login-user", LoginUserView.as_view()),
 ]
